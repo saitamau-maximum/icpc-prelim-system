64ff52d8d844c4532ff63982df41ef3ff8f1867b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 11:32:14 +0900

    feat: create top

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,3 +1,28 @@
+import "./top.css";
+
 export default function top() {
-  return <>top</>;
+  return (
+    <>
+      <p>Team: you</p>
+      <p>Contest is running</p>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>J</td>
+              <td>K</td>
+              <td>L</td>
+              <td>M</td>
+            </tr>
+            <tr className="progress">
+              <td className="blue"></td>
+              <td className="blue"></td>
+              <td className="blue"></td>
+              <td className="blue"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
