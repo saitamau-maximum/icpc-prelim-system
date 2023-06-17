fa41387195af7dffaa3e5ed25d755725202922e9
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 15:23:12 +0900

    feat: create top

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 import "./top.css";
+import Section from "../components/section";
 
 export default function top() {
   return (
     <>
-      <p>Team: you</p>
-      <p>Contest is running</p>
-      <div>
+      <Section title="Stage: rehearsal">
+        <p>The rehearsal is in progress. The rehearsal will end at xx:xx.</p>
+      </Section>
+      <Section title="Team 0: You">
         <table>
           <tbody>
             <tr>
@@ -22,7 +26,48 @@ export default function top() {
             </tr>
           </tbody>
         </table>
-      </div>
+      </Section>
+      <Section title="Problems">
+        <table>
+          <tbody>
+            <tr>
+              <td>All Problems:</td>
+              <td>English</td>
+              <td>/</td>
+              <td>Japanese</td>
+            </tr>
+            <tr>
+              <td>Problem J:</td>
+              <td>English</td>
+              <td>/</td>
+              <td>Japanese</td>
+            </tr>
+            <tr>
+              <td>Problem K:</td>
+              <td>English</td>
+              <td>/</td>
+              <td>Japanese</td>
+            </tr>
+            <tr>
+              <td>Problem L:</td>
+              <td>English</td>
+              <td>/</td>
+              <td>Japanese</td>
+            </tr>
+            <tr>
+              <td>Problem M:</td>
+              <td>English</td>
+              <td>/</td>
+              <td>Japanese</td>
+            </tr>
+          </tbody>
+        </table>
+      </Section>
+      <Section title="Workplace">
+        <p>
+          Get data and send answer <Link to="/workplace">here</Link>
+        </p>
+      </Section>
     </>
   );
 }
