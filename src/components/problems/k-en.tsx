7359731a58951b94f821984cb4e2db754877dd1f
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 18:35:36 +0900

    docs: Problem K

--- src/components/problems/k-en.tsx
+++ src/components/problems/k-en.tsx
@@ -9,6 +9,27 @@ export default function ProblemKEn() {
           the integers given as input.
         </p>
       </Section>
+      <Section title="Input" color="#78f" nomargin>
+        <p>
+          The input is a sequence of one or more lines each containing a single
+          integer. All the integer numbers are positive and less than or equal
+          to 10,000.
+        </p>
+        <p>The end of the input is indicated by a zero</p>
+      </Section>
+      <Section title="Output" color="#78f" nomargin>
+        <p>The largest integer among the integers should be printed.</p>
+      </Section>
+      <Section title="Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`1\n10\n100\n35\n0`}</code>
+        </pre>
+      </Section>
+      <Section title="Output for the Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`100`}</code>
+        </pre>
+      </Section>
     </Section>
   );
 }
--- src/components/problems/k-ja.tsx
+++ src/components/problems/k-ja.tsx
@@ -6,6 +6,27 @@ export default function ProblemKJa() {
       <Section title="最大の整数" color="#78f" nomargin>
         <p>入力として与えられた整数値の最大値を求めなさい。</p>
       </Section>
+      <Section title="Input" color="#78f" nomargin>
+        <p>
+          改行で区切られた 1
+          つ以上の整数が入力として与えられる。すべての整数は正で、 10,000
+          以下である。
+        </p>
+        <p>入力の終わりは 0 で示される。</p>
+      </Section>
+      <Section title="Output" color="#78f" nomargin>
+        <p>最大値を出力しなさい。</p>
+      </Section>
+      <Section title="Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`1\n10\n100\n35\n0`}</code>
+        </pre>
+      </Section>
+      <Section title="Output for the Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`100`}</code>
+        </pre>
+      </Section>
     </Section>
   );
 }
