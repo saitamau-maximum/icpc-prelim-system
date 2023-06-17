186efef338be65b1c8909fa9fcc52a4562ca271e
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 18:38:40 +0900

    docs: Problem K に下線をつける

--- src/components/problems/k-en.tsx
+++ src/components/problems/k-en.tsx
@@ -3,7 +3,12 @@ import Section from "../section";
 export default function ProblemKEn() {
   return (
     <Section title="Problem K" nopadding>
-      <Section title="The Largest Integer" color="#78f" nomargin>
+      <Section
+        title="The Largest Integer"
+        color="#78f"
+        nomargin
+        title_underline
+      >
         <p>
           Your job is to write a program that finds the largest integer among
           the integers given as input.
--- src/components/problems/k-ja.tsx
+++ src/components/problems/k-ja.tsx
@@ -3,7 +3,7 @@ import Section from "../section";
 export default function ProblemKJa() {
   return (
     <Section title="Problem K" nopadding>
-      <Section title="最大の整数" color="#78f" nomargin>
+      <Section title="最大の整数" color="#78f" nomargin title_underline>
         <p>入力として与えられた整数値の最大値を求めなさい。</p>
       </Section>
       <Section title="Input" color="#78f" nomargin>
