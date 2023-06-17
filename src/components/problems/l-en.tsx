d5912e53a33aaa58ee6f104809f2ff63d38cffd8
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 18:38:54 +0900

    docs: Problem L

--- src/components/problems/l-en.tsx
+++ src/components/problems/l-en.tsx
@@ -3,8 +3,38 @@ import Section from "../section";
 export default function ProblemLEn() {
   return (
     <Section title="Problem L" nopadding>
-      <Section title="Multiplication" color="#78f" nomargin>
+      <Section title="Multiplication" color="#78f" nomargin title_underline>
         <p>Calculate the product of 2 integers given as input.</p>
+      </Section>
+      <Section title="Input" color="#78f" nomargin>
+        <p>
+          The input consists of multiple data sets, each in the following
+          format.
+        </p>
+        <pre>
+          <code>{`a b`}</code>
+        </pre>
+        <p>
+          Each of the datasets consists of one line containing two integers{" "}
+          <i>a</i> and <i>b</i>. Both are between 1 and 100,000, inclusive.
+        </p>
+        <p>The end of the input is indicated by a line containing for zeros.</p>
+        <p>The number of datasets does not exceed 100.</p>
+      </Section>
+      <Section title="Output" color="#78f" nomargin>
+        <p>
+          For each of the datasets, output the value of <i>a × b</i>.
+        </p>
+      </Section>
+      <Section title="Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`1 2\n3 4\n5 6\n0 0`}</code>
+        </pre>
+      </Section>
+      <Section title="Output for the Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`2\n12\n30`}</code>
+        </pre>
       </Section>
     </Section>
   );
--- src/components/problems/l-ja.tsx
+++ src/components/problems/l-ja.tsx
@@ -3,8 +3,35 @@ import Section from "../section";
 export default function ProblemLJa() {
   return (
     <Section title="Problem L" nopadding>
-      <Section title="かけざん" color="#78f" nomargin>
+      <Section title="かけざん" color="#78f" nomargin title_underline>
         <p>入力として与えられた整数値の積を求めなさい。</p>
+      </Section>
+      <Section title="Input" color="#78f" nomargin>
+        <p>
+          入力は複数のデータセットからなる。各データセットは次の形式で表される。
+        </p>
+        <pre>
+          <code>{`a b`}</code>
+        </pre>
+        <p>
+          各データセットは a と b の 2 つの整数を含む 1 行からなる。いずれも 1
+          以上 100,000 以下である。
+        </p>
+        <p>入力の終わりは 2 つのゼロを含む行で示す。</p>
+        <p>データセットは 100 個以内である。</p>
+      </Section>
+      <Section title="Output" color="#78f" nomargin>
+        <p>各データセットに対して、 a × b の値を 1 行に出力せよ。</p>
+      </Section>
+      <Section title="Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`1 2\n3 4\n5 6\n0 0`}</code>
+        </pre>
+      </Section>
+      <Section title="Output for the Sample Input" color="#78f" nomargin>
+        <pre>
+          <code>{`2\n12\n30`}</code>
+        </pre>
       </Section>
     </Section>
   );
