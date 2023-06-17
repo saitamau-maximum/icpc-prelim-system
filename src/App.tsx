91fe9a7234eec7f8a1ed96d15e0895009a2124f0
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-17 10:35:20 +0900

    feat: ログイン画面(嘘)を作った

--- src/App.tsx
+++ src/App.tsx
@@ -12,54 +12,58 @@ function App() {
     <div className="App">
       <header className="App-header">Maximum ICPC Practice System</header>
       <div className="container">
-        <section className="App-sidebar">
-          <a href="/top">Top</a>
-          <br />
-          Problems:
-          <table>
-            <tbody>
-              <tr>
-                <td>ALL:</td>
-                <td>En</td>
-                <td>/</td>
-                <td>Ja</td>
-              </tr>
-              <tr>
-                <td>J:</td>
-                <td>En</td>
-                <td>/</td>
-                <td>Ja</td>
-              </tr>
-              <tr>
-                <td>K:</td>
-                <td>En</td>
-                <td>/</td>
-                <td>Ja</td>
-              </tr>
-              <tr>
-                <td>L:</td>
-                <td>En</td>
-                <td>/</td>
-                <td>Ja</td>
-              </tr>
-              <tr>
-                <td>M:</td>
-                <td>En</td>
-                <td>/</td>
-                <td>Ja</td>
-              </tr>
-            </tbody>
-          </table>
-          <a href="/workspace">Workspace</a>
-          <br />
-          <a href="/standings">Standings</a>
-          <br />
-          <a href="/logout">Log Out</a>
-          <br />
-          <a href="/help">Help</a>
-          <br />
-          <a href="/contact">Contact Info</a>
-        </section>
+        {location.pathname === "/" ? (
+          <></>
+        ) : (
+          <section className="App-sidebar">
+            <a href="/top">Top</a>
+            <br />
+            Problems:
+            <table>
+              <tbody>
+                <tr>
+                  <td>ALL:</td>
+                  <td>En</td>
+                  <td>/</td>
+                  <td>Ja</td>
+                </tr>
+                <tr>
+                  <td>J:</td>
+                  <td>En</td>
+                  <td>/</td>
+                  <td>Ja</td>
+                </tr>
+                <tr>
+                  <td>K:</td>
+                  <td>En</td>
+                  <td>/</td>
+                  <td>Ja</td>
+                </tr>
+                <tr>
+                  <td>L:</td>
+                  <td>En</td>
+                  <td>/</td>
+                  <td>Ja</td>
+                </tr>
+                <tr>
+                  <td>M:</td>
+                  <td>En</td>
+                  <td>/</td>
+                  <td>Ja</td>
+                </tr>
+              </tbody>
+            </table>
+            <a href="/workspace">Workspace</a>
+            <br />
+            <a href="/standings">Standings</a>
+            <br />
+            <a href="/">Log Out</a>
+            <br />
+            <a href="/help">Help</a>
+            <br />
+            <a href="/contact">Contact Info</a>
+          </section>
+        )}
         <main>
           <Router>
             <Routes>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import "./index.css";
-
 export default function Index() {
   return (
     <>
-      Team ID: <input type="text" />
+      Team ID:{" "}
+      <input type="text" minLength={6} maxLength={6} pattern="[0-9]{6}" />
+      <br />
+      Password: <input type="password" />
+      <br />
+      <a href="/top">Login</a>
     </>
   );
 }
